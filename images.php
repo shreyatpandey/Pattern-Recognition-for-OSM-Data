8982a7d35a513567759161b504c76f7edc1b6ed0
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2018-04-12 23:51:40 -0700

    Add files via upload

---
+++ images.php
@@ -0,0 +1,88 @@
+<html>
+<body>
+<style>
+.floating-box {
+    float: left;
+    width: auto;
+    height: auto;
+    text.align:center;
+   }
+ .description
+ {
+ 	text-align: center;
+font-weight: normal;
+width: 100px;
+margin: 3px;
+padding: 5px;
+ }
+</style>
+<h1>List of city images</h1>
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\alexandra-gauteng.png" width="100" width="100">
+<div class="description">Alexandra-gauteng</div>
+</div>
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\bisho-easterncape.png" width="100" width="100">
+<div class="description">Easterncape</div>
+</div>
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\cape_town-western_cape.png" width="100" width="100">
+<div class="description">Westerncape</div>
+</div>
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\durban_kwazulu-natal.png" width="100" width="100">
+<div class="description">Durban</div>
+</div>
+
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\johannesburg.png" width="100" width="100">
+<div class="description">Johannesburg</div>
+</div>
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\khayelitsha.png" width="100" width="100">
+<div class="description">Khayelitsha</div>
+
+</div>
+<br><br><br><br>
+</br></br></br></br></br>
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\kimberley.png" width="100" width="100">
+<div class="description">Kimberley</div>
+</div>
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\polokwane.png" width="100" width="100">
+<div class="description">Polokwane</div>
+</div>
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\port_elizabeth.png" width="100" width="100">
+<div class="description">PortElizabeth</div>
+</div>
+
+<div class="floating-box">
+<img src="C:\Users\pande\Desktop\ee599pro\images\pretoria.png" width="100" width="100">
+<div class="description">Pretoria</div>
+</div>
+
+
+</br></br></br></br></br></br></br></br></br>
+<form action="input.php" method="GET">
+City1:<br>
+<input type="text" name="city1">
+<br>
+City2:<br>
+<input type="text" name="city2">
+<br></br>
+<input type="Submit">
+</form>		
+
+
+</body>
+</html>
